b604fee0a2143691b75c935a8e23b75d3c3cbe00
seongwonLee <gbssw13@gmail.com>
2025-01-09 16:28:26 +0900

    fix: cmp BYTE[rdi + rdx],0 -> cmp BYTE[rdi + rcx]. 0

--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -7,18 +7,18 @@ section .text
 _ft_strcpy:
 	push	rbp
 	mov		rbp, rsp
-
+	
 	mov		rcx, -1
 
 .loop:
 	inc		rcx
 
 	mov		BYTE[rdi + rcx], BYTE[rsi + rcx]
-	cmp		BYTE[rdi + rdx], 0
+	cmp		BYTE[rdi + rcx], 0
 	jne		.loop
 
 .done:
 	mov		rax, rdi
 	mov		rsp, rbp
-	pop		rsp
+	pop		rbp
 	ret

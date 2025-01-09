db72627f94e0691a7ebe412cee3f55cbab3a956e
seongwonLee <gbssw13@gmail.com>
2025-01-09 21:11:43 +0900

    add: read, strdup 완료

---
+++ srcs/ft_read.s
@@ -0,0 +1,26 @@
+; ssize_t read(int fildes, void *buf, size_t nbyte);
+; return : 정싱일 때는 읽은 byte, eof 를 읽었으면 0, errno 일 때는 -1
+
+section .text
+    global _ft_read
+    extern ___error
+
+_ft_read:
+    push    rbp
+    mov     rbp, rsp
+    
+    mov     rax, 0x2000003
+    syscall
+    
+    jnc     .done
+
+.error:
+    push    rax
+    call    ___error
+    pop     dword [rax]
+    mov     rax, -1
+    ret
+
+.done:
+    mov     rsp, rbp
+    pop     rbp
---
+++ srcs/ft_strdup.s
@@ -0,0 +1,40 @@
+; char * strdup(const char *s1);
+; return : 새로할당된 char* 주소
+; 에러시 errno 에 ENOMEM 들어가고, NULL 반환
+
+section .text
+    global	_ft_strdup
+	extern	___error
+    extern	malloc
+    extern	free
+	extern	_ft_strlen
+	extern	_ft_strcpy
+
+_ft_strdup:
+	push	rbp
+	mov		rbp, rsp
+	push	rdi
+	
+	call	_ft_strlen
+
+	inc		rax
+	mov		rdi, rax
+
+	call	malloc
+	jc		.error
+
+	pop		rsi
+	mov		rdi, rax
+	call	_ft_strcpy
+	jmp		.done
+
+.error:
+	call	___error
+	mov		[rax], 12
+	xor		rax, rax
+	pop		rdi
+
+.done:
+    mov     rsp, rbp
+    pop     rbp
+    ret
